0a986c81a4613d7ea295a5c9647396c779707c70
Dan Hemerlein <danhemerlein@gmail.com>
2026-05-29 16:13:43 -0400

    add credits.js fiel

---
+++ js/credits.js
@@ -0,0 +1,7 @@
+;(() => {
+  console.log('%cdan.moi', 'font-size: 2rem; font-weight: bold;')
+  console.log('the personal website of dan hemerlein')
+  console.log('coded and vibe coded by dan hemerlein in brooklyn, nyc')
+  console.log('designed by Jordan Layao: https://www.jlayao.com')
+  console.log('3D modeling by Maannik: https://www.quipjar.com/video')
+})()
